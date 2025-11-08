c35678816435948d1d11b284ac6e52044e60cdff
Code Expert <codeexpert07@gmail.com>
2025-11-08 21:54:53 +0530

    Fix test cases - gemini

--- common-library/src/main/java/com/codeexpert/common/command/BaseCommand.java
+++ common-library/src/main/java/com/codeexpert/common/command/BaseCommand.java
@@ -1,12 +1,26 @@
 package com.codeexpert.common.command;
 
-import com.codeexpert.common.event.DomainEvent; // Added import
+import com.codeexpert.common.event.DomainEvent;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import lombok.Data;
 import lombok.experimental.SuperBuilder;
 
 @Data
 @SuperBuilder
-public class BaseCommand implements DomainEvent { // Implements DomainEvent
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.PROPERTY,
+        property = "commandType"
+)
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = CreateShipmentCommand.class, name = "CreateShipmentCommand"),
+        @JsonSubTypes.Type(value = ProcessPaymentCommand.class, name = "ProcessPaymentCommand"),
+        @JsonSubTypes.Type(value = RefundPaymentCommand.class, name = "RefundPaymentCommand"),
+        @JsonSubTypes.Type(value = ReleaseInventoryCommand.class, name = "ReleaseInventoryCommand"),
+        @JsonSubTypes.Type(value = ReserveInventoryCommand.class, name = "ReserveInventoryCommand")
+})
+public class BaseCommand implements DomainEvent {
 
     private String orderId;
 
